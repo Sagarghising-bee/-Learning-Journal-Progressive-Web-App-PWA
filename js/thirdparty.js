fc6afbb0375bee5b2703d87f8416b3e3fa192c7a
Sagarghising-bee <sagarghising359@gmail.com>
2025-11-05 20:27:27 +0000

    Create thirdparty.js lab 4 Completed

---
+++ js/thirdparty.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const videoContainer = document.getElementById("youtube-video");
+  if (videoContainer) {
+    videoContainer.innerHTML = `
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    `;
+  }
+});
